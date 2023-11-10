48a70a834aad3ecf50171a8f83c0d2fb2591daf7
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2023-11-10 11:29:40 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import NotableItems from "@/app/components/feature/NotableItems";
 import RQIS from "@/app/components/feature/RQIS";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <NotableItems src={"https://fakestoreapi.com/products"}/>
+      <Link href={"https://github.com/lif31up/shopping-mall"}>{"리포지토리로 이동하기"}</Link>
+      <Link href={"https://fakestoreapi.com"}>{"fakestoreapi 서버 상태 확인하기"}</Link>
       <RQIS src={"https://fakestoreapi.com/products"}/>
     </main>
   )
